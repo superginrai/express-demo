7ad764b562a4769dd701e2ed06e67c4d0ee39f61
Cory Booth <Rodimus2005@hotmail.com>
2018-04-17 11:52:30 -0500

    add get requests

--- server.js
+++ server.js
@@ -3,4 +3,14 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-app.listen(PORT);
+app.get('/', function (req, res) {
+    res.send('Hello Tacos');
+})
+
+app.get('/hey', function (req, res){
+    res.send('Howdy Tacos');
+})
+
+app.listen(PORT, function () {
+    console.log(`listening on port ${PORT}`)
+});
